6c57f5d0db608276ca82a7ff948e10bbe91a7a98
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-11-19 15:11:30 -0500

    Ejercicio 1

---
+++ ejercicios.js
@@ -0,0 +1,6 @@
+const parrafo3 = document.getElementById('parrafo-ej3');
+const boton3 = document.getElementById('boton-ej3');
+
+boton3.addEventListener('click', function() {
+    parrafo3.textContent = 'Texto cambiado';
+});
