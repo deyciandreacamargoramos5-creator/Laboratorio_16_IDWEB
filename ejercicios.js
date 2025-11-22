fe4f471ce8a7641c92ad67732d2ed45f8e8466ba
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-11-21 19:14:29 -0500

    Ejercicio 4

--- ejercicios.js
+++ ejercicios.js
@@ -5,3 +5,18 @@ const boton3 = document.getElementById('boton-ej3');
 boton3.addEventListener('click', function() {
     parrafo3.textContent = 'Texto cambiado';
 });
+//Ejercicio 4
+const parrafo4 = document.getElementById('parrafo-ej4');
+const boton4 = document.getElementById('boton-ej4');
+
+let textoAlternado = false; 
+
+boton4.addEventListener('click', function() {
+    if (textoAlternado) {
+        parrafo4.textContent = 'Texto original';
+    } else {
+        parrafo4.textContent = 'Texto cambiado';
+    }
+    textoAlternado = !textoAlternado;
+});
+
